b08ac45e6e6dee06a8bfadcb43e68f89335ba36b
zach <huffyh00@hotmail.com>
2020-10-12 21:57:51 -0500

    Add: Validaciones de formulario basicas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import SignigSingUp from "./page/SigninSingUp";
+import { ToastContainer } from 'react-toastify';
 
 export default function App() {
   const [ user, setUser ] = useState({name:"Zach"});
@@ -14,6 +15,16 @@ export default function App() {
       ) : (
         <h1>No Estas Logueado</h1>
       )}
+      <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnVisibilityChange
+      draggable
+      pauseOnHover
+      />
     </div>
   );
 }
--- src/components/SingUpForm/SingUpForm.js
+++ src/components/SingUpForm/SingUpForm.js
@@ -1,34 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col, Form, Button, Spinner } from "react-bootstrap";
 import "./SingUpForm.scss";
+import {values, size} from "lodash";
+import { toast } from 'react-toastify';
 
 export default function SingUpForm(props) {
   const { setShowModal } = props;
 
+  const [formData, setFormData] = useState(inicialFormValue());
+
   const onSubmit = (evento) => {
     evento.preventDefault();
-    setShowModal(false);
+    
+    console.log(formData)
+
+    let validCount = 0;
+
+    values(formData).some(value =>{
+      value && validCount ++
+      return null
+    })
+
+      if(validCount !== size(formData)){
+        toast.warning("Completa todos los campos del formulario")
+      }else{
+
+        //if()
+        toast.success("formulario ok")
+      }
+
+    console.log(validCount)
+
+  };
+
+  const onChange = (evento) => {
+    console.log(evento.target.name);
+    setFormData({ ...formData, [evento.target.name]: evento.target.value });
   };
 
   return (
     <div className="sing-up-form">
       <h2>Crea Tu Cuenta.</h2>
 
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={onSubmit} onChange={onChange}>
         <Form.Group>
           <Row>
             <Col>
-              <Form.Control type="text" placeholder="Nombres"></Form.Control>
+              <Form.Control
+                type="text"
+                placeholder="Nombres"
+                defaultValue={formData.nombres}
+                name="nombres"
+              ></Form.Control>
             </Col>
 
             <Col>
-              <Form.Control type="text" placeholder="Apellidos"></Form.Control>
+              <Form.Control
+                type="text"
+                placeholder="Apellidos"
+                defaultValue={formData.apellidos}
+                name="apellidos"
+              ></Form.Control>
             </Col>
           </Row>
         </Form.Group>
 
         <Form.Group>
-          <Form.Control type="email" placeholder="Email"></Form.Control>
+          <Form.Control
+            type="email"
+            placeholder="Email"
+            defaultValue={formData.email}
+            name="email"
+          ></Form.Control>
         </Form.Group>
 
         <Form.Group>
@@ -37,12 +80,16 @@ export default function SingUpForm(props) {
               <Form.Control
                 type="password"
                 placeholder="Password"
+                defaultValue={formData.password}
+                name="password"
               ></Form.Control>
             </Col>
             <Col>
               <Form.Control
                 type="password"
                 placeholder="Repetir Password"
+                defaultValue={formData.repeatPassword}
+                name="repeatPassword"
               ></Form.Control>
             </Col>
           </Row>
@@ -54,4 +101,14 @@ export default function SingUpForm(props) {
       </Form>
     </div>
   );
+}
+
+function inicialFormValue() {
+  return {
+    nombres: "",
+    apellidos: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+  };
 }
