d1cf87e814fa05f6f0c14a57f28a63beb758e430
zach <huffyh00@hotmail.com>
2020-10-13 21:17:33 -0500

    Add: Registro de usuarios en base de datos conexion con API GOLANG BACK-END

--- src/components/SingUpForm/SingUpForm.js
+++ src/components/SingUpForm/SingUpForm.js
@@ -1,40 +1,65 @@
 import React, { useState } from "react";
 import { Row, Col, Form, Button, Spinner } from "react-bootstrap";
 import "./SingUpForm.scss";
-import {values, size} from "lodash";
-import { toast } from 'react-toastify';
+import { values, size } from "lodash";
+import { toast } from "react-toastify";
+import { isEmailValid } from "../../utils/validations";
+import  {signUpApi} from "../../api/auth";
 
 export default function SingUpForm(props) {
   const { setShowModal } = props;
 
   const [formData, setFormData] = useState(inicialFormValue());
 
+  const [singUpLoanding, setSingUpLoanding] = useState(false);
+
+
   const onSubmit = (evento) => {
     evento.preventDefault();
+
     
-    console.log(formData)
+
 
     let validCount = 0;
 
-    values(formData).some(value =>{
-      value && validCount ++
-      return null
-    })
+    values(formData).some((value) => {
+      value && validCount++;
+      return null;
+    });
 
-      if(validCount !== size(formData)){
-        toast.warning("Completa todos los campos del formulario")
-      }else{
-
-        //if()
-        toast.success("formulario ok")
+    if (validCount !== size(formData)) {
+      toast.warning("Completa todos los campos del formulario");
+    } else {
+      if (!isEmailValid(formData.email)) {
+        toast.warning("Email Invalido");
+        
+      } else if (formData.password !== formData.repeatPassword) {
+        toast.warning("Las Contraseñas no coinciden");
+      } else if (size(formData.password) < 6) {
+        toast.warning("La Contraseña tiene que tener al menos 6 caracteres ");
+      } else {
+        setSingUpLoanding(true);
+        signUpApi(formData).then(response=>{
+          if (response.code){
+            toast.warning(response.message)
+          }else{
+            toast.success("El registro ah sido correcto")
+            setShowModal(false)
+            setFormData(inicialFormValue)
+          }
+        }).catch(() =>{
+          toast.error("Error del Servidor , intentelo mas tarde")
+        }).finally(()=>{
+          setSingUpLoanding(false)
+        })
       }
+    }
 
-    console.log(validCount)
-
+    
   };
 
   const onChange = (evento) => {
-    console.log(evento.target.name);
+    
     setFormData({ ...formData, [evento.target.name]: evento.target.value });
   };
 
@@ -96,7 +121,8 @@ export default function SingUpForm(props) {
         </Form.Group>
 
         <Button variant="primary" type="submit">
-          Registrarse
+            {!singUpLoanding ? "Registrate" : <Spinner animation="border"/>}
+          
         </Button>
       </Form>
     </div>
